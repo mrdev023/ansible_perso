3132cd898bd10c0936f695d02e675243ac9ec96b
Florian RICHER <florian.richer@unova.fr>
2023-08-09 22:50:38 +0200

    Migrate to https from script

--- configure.sh
+++ configure.sh
@@ -2,7 +2,7 @@
 
 sudo dnf install -y ansible
 ansible-galaxy collection install community.general
-git -C ~/.ansible_perso pull || git clone -j8 git@github.com:mrdev023/ansible_perso.git -b fedora ~/.ansible_perso
+git -C ~/.ansible_perso pull || git clone -j8 https://github.com/mrdev023/ansible_perso.git -b fedora ~/.ansible_perso
 cd ~/.ansible_perso
 ansible-playbook -i inventory/localhost playbook.yml --ask-become-pass
 cd -
