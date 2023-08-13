3829e46ecb48c124e2fe099b31f374570ef35747
Florian RICHER <florian.richer@unova.fr>
2023-08-13 14:13:06 +0200

    Add vars + autoconfigure vars

--- configure.sh
+++ configure.sh
@@ -1,8 +1,24 @@
-#!/bin/sh
+#!/bin/bash
 
+# Prepare system
 sudo dnf install -y ansible
 ansible-galaxy collection install community.general
+
+# Get or update repository
 git -C ~/.ansible_perso pull || git clone -j8 https://github.com/mrdev023/ansible_perso.git -b fedora ~/.ansible_perso
+
+# Go to repo
 cd ~/.ansible_perso
+
+vars=$(ls group_vars/all.*)
+select var in "${vars[@]}"
+do
+  \cp -fv "$var" ./group_vars/all
+  break
+done
+
+# Run
 ansible-playbook -i inventory/localhost playbook.yml --ask-become-pass
+
+# Restore old pwd
 cd -
