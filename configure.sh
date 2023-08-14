7fe0b5666c91f69313a968693b49e93b5036cc40
Florian Richer <florian.richer.97@outlook.com>
2023-08-14 13:55:38 +0200

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-select var in $(ls group_vars/all.*)
-do
-  \cp -fv "$var" ./group_vars/all
-  break
-done
-
 # Prepare system
 sudo dnf install -y ansible
 ansible-galaxy collection install community.general
@@ -15,6 +9,12 @@ git -C ~/.ansible_perso pull || git clone -j8 https://github.com/mrdev023/ansibl
 
 # Go to repo
 cd ~/.ansible_perso
+
+select var in $(ls group_vars/all.*)
+do
+  \cp -fv "$var" ./group_vars/all
+  break
+done
 
 # Run
 ansible-playbook -i inventory/localhost playbook.yml --ask-become-pass
